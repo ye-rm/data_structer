ea1c51f681dc0d4ba6293bbc58c17d10633ded91
ye-rm <wenyuye@bupt.edu.cn>
2022-10-24 15:09:50 +0800

    init

---
+++ binary_tree.c
@@ -0,0 +1,5 @@
+//
+// Created by 98770 on 2022/10/24.
+//
+
+#include "binary_tree.h"
---
+++ binary_tree.h
@@ -0,0 +1,11 @@
+//
+// Created by 98770 on 2022/10/24.
+//
+
+#ifndef DATA_STRUCTER_BINARY_TREE_H
+#define DATA_STRUCTER_BINARY_TREE_H
+#define Status int
+
+Status
+
+#endif //DATA_STRUCTER_BINARY_TREE_H
